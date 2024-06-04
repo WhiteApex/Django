ed515929f012ecdac3e8954e9465a96f1206e871
WhiteApex <87219932+WhiteApex@users.noreply.github.com>
2024-06-04 13:10:09 +0500

    step forward by setting up product contexts

--- store/urls.py
+++ store/urls.py
@@ -17,8 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("main.urls"), name='main')
+    path('', include("main.urls", namespace='main')),
+    path('catalog/', include("goods.urls", namespace='catalog'))
 ]
-
